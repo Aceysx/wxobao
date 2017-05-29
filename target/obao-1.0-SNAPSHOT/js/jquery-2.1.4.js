e0edd9b89cb825cd69e56f2bec051752044b29cd
Aceysx <aceysx@163.com>
2017-05-29 16:58:56 +0800

    add index.jsp

--- target/obao-1.0-SNAPSHOT/js/jquery-2.1.4.js
+++ target/obao-1.0-SNAPSHOT/js/jquery-2.1.4.js
@@ -2711,7 +2711,7 @@ jQuery.fn.extend({
 			this,
 
 			// If this is a positional/relative selector, check membership in the returned set
-			// so $("p:first").is("p:last") won't return true for a doc with two "p".
+			// so $("p:first").is("p:last") won't return true for a 文档 with two "p".
 			typeof selector === "string" && rneedsContext.test( selector ) ?
 				jQuery( selector ) :
 				selector || [],
